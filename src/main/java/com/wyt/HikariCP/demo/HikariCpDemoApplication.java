929c29a3cef0c558d379b7bf246c994b3f7f8cf7
wangyitao <182443045@qq.com>
2019-03-26 18:48:36 +0800

    spring boot基础代码

---
+++ src/main/java/com/wyt/HikariCP/demo/HikariCpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyt.HikariCP.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariCpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikariCpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyt/HikariCP/demo/HikariCpDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wyt.HikariCP.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HikariCpDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
